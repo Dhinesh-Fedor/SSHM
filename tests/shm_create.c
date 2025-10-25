33a5d4fbb9fff7dc3136970ad186460206194326
Dhinesh-Fedor <dhineshkumar2341@gmail.com>
2025-10-25 23:41:18 +0530

    added lib implementations

--- tests/shm_create.c
+++
@@ -1,7 +0,0 @@
-#include "sshm.h"
-
-
-int main(){
-    char name[20] = "/test1";
-    sshm_create(name, O_CREAT | O_EXCL | O_RDWR, 0600, 2048);
-}
--- tests/shm_o_exist.c
+++
@@ -1,8 +0,0 @@
-#include "sshm.h"
-
-
-int main(){
-    char name[20] = "/test1";
-    uint32_t magic = 0xDEADBEEF;
-    sshm_o_exist(name,magic);
-}
